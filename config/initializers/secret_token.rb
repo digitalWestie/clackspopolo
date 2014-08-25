f49ed4da8f36f4d91774bf6938e8ae5ac79c1080
Rory Gianni <giannichan@gmail.com>
2014-08-25 20:40:45 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClacksPopolo::Application.config.secret_token = 'a628d5d04513b6a4d37b8d020151a1954b9216d8d075fbfd1f7771c0c8c5177801ff561a1e47d57769693bd0fc36a03dd8cbafd5c7ee17732b0456df21afe17c'
