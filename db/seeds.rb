6cdff53e527b8f103a8a61e71d220a83fc27d1ae
Rory Gianni <giannichan@gmail.com>
2014-10-15 18:12:34 +0100

    renames party

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Popolo::Post.destroy_all
 council = Popolo::Organization.create(name: 'Clackmannanshire Council', classification: 'Local Authority')
 
 labour = Popolo::Organization.create(name:'Labour Party', classification: 'Political Party')
-tories = Popolo::Organization.create(name:'Conservatives', classification: 'Political Party')
+tories = Popolo::Organization.create(name:'Conservative Party', classification: 'Political Party')
 nats   = Popolo::Organization.create(name:'SNP', classification: 'Political Party')
 libdems   = Popolo::Organization.create(name:'Liberal Democrats', classification: 'Political Party')
 greens = Popolo::Organization.create(name:'Green Party', classification: 'Political Party') # no greens...
