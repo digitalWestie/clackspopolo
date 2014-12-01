f5036ab51a1002b303d6622a5867a15c5368090e
Rory Gianni <giannichan@gmail.com>
2014-12-01 20:10:53 +0000

    allow contact details to be nested in organizations form

--- app/models/organization_extensions.rb
+++ app/models/organization_extensions.rb
@@ -4,6 +4,7 @@ module OrganizationExtensions
   included do
     accepts_nested_attributes_for :links
     accepts_nested_attributes_for :sources
+    accepts_nested_attributes_for :contact_details
 
     singleton_class.prepend ClassMethods
     prepend InstanceMethods
