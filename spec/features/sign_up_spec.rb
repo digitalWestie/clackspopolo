1d3fcc1b0ffad77b4ec5b7289b027ddea599131d
digitalWestie <giannichan@gmail.com>
2015-03-10 19:27:04 +0000

    adds a proper sign up test and way to clear users before tests are run

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 feature 'As a user who wants to add content to the site' do
-  #let!(:user) { FactoryGirl.create(:user) }
-  scenario 'I cannot access organizations until I have signed up and signed in', js:true do
-    visit '/admin/organizations/'
-    expect(current_path).to eql(new_admin_session_path)
+  #let!(:admin) { FactoryGirl.create(:admin) }
+
+  scenario 'can sign up with valid input', js:false do
+    visit '/admin/sign_up/'
+
+    fill_in 'Email',                      with: 'jimmy@example.com'
+    fill_in 'admin_password',              with: 'passwurd'
+    fill_in 'admin_password_confirmation', with: 'passwurd'
+    click_button 'Sign up'
+
+    expect(Admin.where(email: "jimmy@example.com").exists?).to eql(true)
+    expect(current_path).to eql(root_path)
   end
+
 end
--- spec/features/step_definitions/user_steps.rb
+++ spec/features/step_definitions/user_steps.rb
@@ -1,4 +1,4 @@
 Given /^I am logged in as a user$/ do
-  @current_user = User.create!(:username => 'user', :password => 'password')
-  login_as(@current_user, :scope => :user)
+  @current_user = Admin.create!(:email => 'example@example.com', :password => 'password')
+  login_as(@current_user, :scope => :admin)
 end
